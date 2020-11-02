6909b32858351564efe3fcb48227d8913060b57b
root <3446602239@qq.com>
2020-11-02 17:56:49 +0800

    git commit -m "c5"

--- Pythonds/Chapter5.py
+++ Pythonds/Chapter5.py
@@ -14,8 +14,24 @@ def listsum2(numlist):
     if len(numlist)==1:
         return numlist[0]
     else:
-        return numlist[0]+listsum(numlist[1:])
+        return numlist[0]+listsum2(numlist[1:])
+    
+# %%
+print(listsum2([i for i in range(100)]))
+# %%
+import sys
+sys.setrecursionlimit(1000000)
+
+
+# %% convert an integer to a string in any base
+def toStr(n,base):
+    convertString="0123456789ABCDEF"
+    if n<base:
+        return convertString[n]
+    else:
+        return toStr(n//base,base)+convertString[n%base]
+
 
 # %%
-print(listsum2([1,3,5,7,9]))
+print(toStr(1453,16))
 # %%
